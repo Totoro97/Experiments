05521bbe267ff2b7eba7bf0c453d691d2dfc727d
totoro97 <totoro97@outlook.com>
2018-11-14 16:06:06 +0800

    gradient optimize?

--- ReconstructThin/src/Bend.cpp
+++ ReconstructThin/src/Bend.cpp
@@ -13,12 +13,26 @@ void Bend::GoBendNow() {
   for (auto &curve : curves_) {
     double past_cost = 1e15;
     for (iter_num = 100; iter_num > 0; iter_num--) {
-      
-      for (const auto &map2d : map2ds_) {
-        for (auto &pt : curve.points_) {
-          double single_dis = map2d.MinDist2Edge(pt);
-          
+      for (auto &pt : curve.points_) {
+        double current_dis = 0.0;
+        for (const auto &map2d : map2ds_) {
+          double single_dis = map2d.MinDist2Edge(pt);  
+          current_dis += single_dis;
         }
+        Eigen::Vector3d grad(0.0, 0.0, 0.0);
+        for (int index = 0; index < 3; index++) {
+          double new_dis = 0.0;
+          const double step_length = 1e-3;
+          Eigen::Vector3d bias(0.0, 0.0, 0.0);
+          bias(i) = step_length;
+          for (const auto &map2d: map2ds_) {
+            double single_dis = map2d.MinDist2Edge(pt + bias);
+            new_dis += single_dis;  
+          }
+          grad(i) = (new_dis - current_dis) / step_length;
+        }
+        const double step_ratio = 1.0;
+        pt = pt + grad * step_ratio;
       }
     }
   }
